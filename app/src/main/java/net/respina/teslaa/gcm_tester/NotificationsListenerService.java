2688f7cb2db787ab5758bfec1e73d1e5808a7fe5
amir <Blackworld.2035@gmail.com>
2017-11-07 13:47:29 +0330

    GCM updated to the latest version

---
+++ app/src/main/java/net/respina/teslaa/gcm_tester/NotificationsListenerService.java
@@ -0,0 +1,7 @@
+package net.respina.teslaa.gcm_tester;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+public class NotificationsListenerService extends GcmListenerService {
+
+}
---
+++ app/src/main/java/net/respina/teslaa/gcm_tester/RegistrationService.java
@@ -0,0 +1,39 @@
+package net.respina.teslaa.gcm_tester;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GcmPubSub;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
+
+import de.greenrobot.event.EventBus;
+
+public class RegistrationService extends IntentService {
+
+    public RegistrationService() {
+        super("RegistrationService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        InstanceID myID = InstanceID.getInstance(this);
+        try {
+            String registrationToken = myID.getToken(
+                    "787112732592",
+                    GoogleCloudMessaging.INSTANCE_ID_SCOPE,
+                    null
+            );
+
+            Log.d("Registration Token", registrationToken);
+
+            GcmPubSub subscription = GcmPubSub.getInstance(this);
+            subscription.subscribe(registrationToken, "/topics/my_little_topic", null);
+
+        }catch(Exception e){
+            Log.e("REGISTRATION ERROR", e.toString());
+        }
+    }
+}
